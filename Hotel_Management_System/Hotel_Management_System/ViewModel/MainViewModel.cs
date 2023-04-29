59241b84b81ae0f5c2a9640f191f302601a2ef04
Huynh Tien Phat <2409huynhphat@gmail.com>
2023-04-29 15:06:34 +0700

    Finish logic and UI LoginForm

---
+++ Hotel_Management_System/Hotel_Management_System/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Hotel_Management_System.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Hotel_Management_System.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool Isloaded = false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public MainViewModel()
+        {
+            LoadedWindowCommand = new RelayCommand<Window>((p) => { return true; }, (p) =>
+            {
+                Isloaded = true;
+
+                if (p == null) return;
+                p.Hide();
+
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+
+                if (loginWindow.DataContext == null) return;
+                var loginVM = loginWindow.DataContext as LoginViewModel;
+
+                if(loginVM.IsLogin)
+                {
+                    p.Show();
+                }
+                else
+                {
+                    p.Close();
+                }
+            });
+        }
+    }
+}
