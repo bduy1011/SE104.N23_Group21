82a6c844eb09954121b30148a4a263db03eb5cc5
bduy1011 <nguyentruongbaoduy2003@gmail.com>
2023-06-07 02:04:21 +0700

    Add function check information when booking room

---
+++ Hotel_Management_System/Hotel_Management_System/Hotel_Management_System/View/RoomMapView/RoomMapView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Hotel_Management_System.Model;
+using Hotel_Management_System.ViewModel.Other;
+
+namespace Hotel_Management_System.View.RoomMapView
+{
+    /// <summary>
+    /// Interaction logic for RoomMapView.xaml
+    /// </summary>
+    public partial class RoomMapView : UserControl
+    {
+        public RoomMapView()
+        {
+            InitializeComponent();
+        }
+    }
+}
